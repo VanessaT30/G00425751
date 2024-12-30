dc927514099b90f9f7672c23e7e3cc30adaba06d
Vanessa Tolentino <VTolentino@gmail.com>
2024-12-30 14:15:45 +0000

    Edit Home Page, no settings yet

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IonButton, IonContent, IonHeader, IonInput, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { MyDataService } from '../services/my-data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  keyword:string = "";
+  
+  constructor(private router: Router, private ds: MyDataService) {}
+
+  async openCountry() {
+    await this.ds.set("kw", this.keyword)
+    this.router.navigate(['/movies'])
+    console.log(this.keyword);
+  }
+
 }
--- src/app/services/my-data.service.ts
+++ src/app/services/my-data.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyDataService {
 
-  constructor() { }
+  constructor(private storage: Storage) { 
+    this.init();
+  }
+
+  // Creates the storage
+  async init() {
+    await this.storage.create();
+  }
+
+  // Puts something into storage
+  async set(key: string, value:any) {
+    await this.storage.set(key, value);
+  }
+
+  // Gets something out of storage
+  async get(key: string) {
+    return await this.storage.get(key);
+  }
+
+
+
 }
